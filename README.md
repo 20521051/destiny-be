71b4b251071fccc234b92c4ff70e63eac0f96626
Hoàng An <20521051@gm.uit.edu.vn>
2023-05-17 14:57:17 +0700

    refactor: add some package

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Khởi tạo project
 
 ```
-npm init
 
-yarn add @types/express @types/mongoose  @types/cors dotenv @types/body-parser @types/morgan
 ```
---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,52 @@
+import * as express from "express";
+import * as jwt from "jsonwebtoken";
+import { UsersService } from "../services";
+import HttpException from "../utils/http-exception";
+import { HttpStatus } from "../constances/enum";
+
+export function expressAuthentication(
+  request: express.Request,
+  securityName: string,
+  scopes?: string[]
+) {
+  const token = request.headers["authorization"]?.split(" ")[1] || "";
+
+  return new Promise((resolve, reject) => {
+    if (!token) {
+      reject(new HttpException(HttpStatus.UNAUTHORIZED, "No token provided"));
+    }
+    // @typescript-eslint/no-explicit-any
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "",
+      async function (err: any, decoded: any) {
+        if (err) {
+          return reject(
+            new HttpException(HttpStatus.UNAUTHORIZED, err.message)
+          );
+        }
+
+        const user = await UsersService.findUserById(decoded.userId);
+        if (!user) {
+          return reject(
+            new HttpException(HttpStatus.NOT_FOUND, "User not exist")
+          );
+        }
+        // Check if JWT contains all required scopes
+        if (scopes && scopes.length > 0) {
+          for (const scope of scopes) {
+            if (!decoded.role.includes(scope)) {
+              return reject(
+                new HttpException(
+                  HttpStatus.FOBIDDEN,
+                  "JWT does not contain required role."
+                )
+              );
+            }
+          }
+          resolve(decoded);
+        }
+      }
+    );
+  });
+}
