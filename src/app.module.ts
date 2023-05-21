92c8d5edcd1f58782a961e1bcabae37dfccd9b29
Hoàng An <20521051@gm.uit.edu.vn>
2023-05-21 21:54:25 +0700

    fix: mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
 import { AuthModule, UserModule } from './modules';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration]
+      envFilePath: '.env',
+      isGlobal: true
     }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     AuthModule,
     UserModule
   ],
